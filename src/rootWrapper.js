d85d4564b458d3f11fcdfd2922f17ac893c0e133
Iduart <iduartdean@gmail.com>
2019-07-27 17:28:52 -0500

    setup apollo client

--- src/rootWrapper.js
+++ src/rootWrapper.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from "./store/store";
+import React from "react"
+import { Provider } from "react-redux"
+import { ApolloProvider } from "react-apollo-hooks";
+import store from "./store/store"
+import { client } from "./apollo/client"
 
 export default ({ element }) => (
-  <Provider store={store}>{element}</Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>{element}</Provider>
+  </ApolloProvider>
 )
